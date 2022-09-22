213cb1b5a1a05a751bb670b36032052468f75f7f
LucioFurnari <luciofurnari99@gmail.com>
2022-09-22 17:28:09 -0300

    Add a simple event to draw the grid

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const gridContainer = document.querySelector(".grid-container");
 let numberOfColumns = 16;
 let numberOfRows = 16;
+let isDrawing = false;
 gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumns},1fr)`;
 gridContainer.style.gridTemplateRows = `repeat(${numberOfRows},1fr)`;
 
 for(let i=0; i< numberOfColumns*numberOfRows;i++){
     let square = document.createElement(`div`);
     square.classList.add("square");
-    square.addEventListener("mousedown",(event) => console.log(event))
-    square.addEventListener("mouseup",(event) => console.log(event))
+    square.addEventListener("mousedown",(event) => {
+        isDrawing = true;
+        console.log(event.target);
+    })
+    square.addEventListener("mousemove",(event) => {
+        if(isDrawing){
+            event.target.style.backgroundColor="black";
+        }
+    })
+    square.addEventListener("mouseup",(event) => {
+        isDrawing = false;
+    })
     gridContainer.appendChild(square);
 }
 
+document.addEventListener("mousedown",(e) => console.log(e.target.className))
 function drawSquares(){
 
 }
