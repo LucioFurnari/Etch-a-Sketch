5ada582f9283af5126da03830504f116dcda69b5
LucioFurnari <luciofurnari99@gmail.com>
2022-09-23 20:10:41 -0300

    Add a function to change the color of the pencil

--- script.js
+++ script.js
@@ -1,29 +1,73 @@
-const gridContainer = document.querySelector(".grid-container");
-let numberOfColumns = 16;
-let numberOfRows = 16;
-let isDrawing = false;
-gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumns},1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${numberOfRows},1fr)`;
+const gridContainer = document.querySelector(".grid-container"); //Grid container of the squares.
+const colorContainer = document.querySelector(".show-color");//Display the color in the page.
+const numberInput = document.querySelector(".number-input");
 
-for(let i=0; i< numberOfColumns*numberOfRows;i++){
+const inputHueColor = document.querySelector(".input-hue");
+const inputSaturation = document.querySelector(".input-saturation");
+const inputLightness = document.querySelector(".input-lightness");
+
+let numberOfColumnsAndRows = 16;
+// let numberOfRows = 16;
+let isDrawing = false;
+
+let hueColor = inputHueColor.value;
+let saturationColor = inputSaturation.value;
+let lightnessColor = inputLightness.value;
+let hslColor =`hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`;
+colorContainer.style.backgroundColor =  hslColor;
+
+inputHueColor.addEventListener("input", createHslColor);
+inputSaturation.addEventListener("input", createHslColor);
+inputLightness.addEventListener("input", createHslColor );
+
+function createHslColor(event){
+    
+    if(event.target.className === "input-hue"){
+        hueColor = event.target.value;
+    } else if (event.target.className === "input-saturation"){
+        saturationColor = event.target.value;
+    } else {
+        lightnessColor = event.target.value;
+    }
+    console.log(hueColor);
+    console.log(saturationColor);
+    hslColor = `hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`
+    colorContainer.style.backgroundColor = hslColor;
+}
+
+numberInput.addEventListener("input",(event) => {
+    drawSquares(gridContainer,event.target.value);
+    gridContainer.style.gridTemplateColumns = `repeat(${event.target.value},1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${event.target.value},1fr)`;
+});
+gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumnsAndRows},1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${numberOfColumnsAndRows},1fr)`;
+
+drawSquares(gridContainer,numberOfColumnsAndRows) //Draw the squares in the grid.
+function drawSquares(parent,number){ 
+    while (parent.hasChildNodes()) {         ////Clean the Grid of squares before create new ones.
+        parent.removeChild(parent.firstChild)   
+    }
+    for(let i=0; i< number*number;i++){ ///Create squares in the grid with a loop.
     let square = document.createElement(`div`);
     square.classList.add("square");
-    square.addEventListener("mousedown",(event) => {
-        isDrawing = true;
-        console.log(event.target);
-    })
-    square.addEventListener("mousemove",(event) => {
+    square.addEventListener("mousemove",(event) => { //Event to paint the squares, if "isDrawing" is true.
         if(isDrawing){
-            event.target.style.backgroundColor="black";
+            event.target.style.backgroundColor= hslColor;
         }
     })
-    square.addEventListener("mouseup",(event) => {
+    parent.appendChild(square);
+    }
+}
+
+document.addEventListener("mousedown",(event) => {
+    if(event.target.className == "square"){
+        isDrawing = true;
+    } else {
         isDrawing = false;
-    })
-    gridContainer.appendChild(square);
-}
+    }
+})
+document.addEventListener("mouseup",() => {
+    isDrawing = false;
+})
 
-document.addEventListener("mousedown",(e) => console.log(e.target.className))
-function drawSquares(){
-
-}
