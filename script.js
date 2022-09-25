5ca12158562bd5378a06f02a7faeffe66abb5b31
LucioFurnari <luciofurnari99@gmail.com>
2022-09-25 20:47:24 -0300

    Add rainbow color mode

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const gridContainer = document.querySelector(".grid-container"); //Grid containe
 const colorContainer = document.querySelector(".show-color");//Display the color in the page.
 // const numberInput = document.querySelector(".number-input");
 const btnGrid = document.querySelector(".grid-btn");
+const btnRainbow = document.querySelector(".rainbow-btn");
 
 const inputHueColor = document.querySelector(".input-hue");
 const inputSaturation = document.querySelector(".input-saturation");
@@ -9,12 +10,14 @@ const inputLightness = document.querySelector(".input-lightness");
 
 let numberOfColumnsAndRows = 16;
 let isDrawing = false;
+let isRainbowMode = false;
 
-let hueColor = inputHueColor.value;
-let saturationColor = inputSaturation.value;
-let lightnessColor = inputLightness.value;
-let hslColor =`hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`;
-colorContainer.style.backgroundColor =  hslColor;
+let hueColor = inputHueColor.value; // Save the hue color.
+let saturationColor = inputSaturation.value; // Save the saturation.
+let lightnessColor = inputLightness.value; // Save the lightness.
+let hslColor =`hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`; // Save the actual color selected
+
+colorContainer.style.backgroundColor =  hslColor; // Show the actual color in the box.
 
 inputHueColor.addEventListener("input", createHslColor);
 inputSaturation.addEventListener("input", createHslColor);
@@ -22,7 +25,7 @@ inputLightness.addEventListener("input", createHslColor );
 
 btnGrid.addEventListener("click",() => {
     let changeSquares = parseInt(prompt("Ingrese la cantidad de squares (Maximo 100)"));
-    if(changeSquares <= 100) {
+    if(changeSquares <= 100 && changeSquares >= 2) {
         drawSquares(gridContainer,changeSquares);
         gridContainer.style.gridTemplateColumns = `repeat(${changeSquares},1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${changeSquares},1fr)`;
@@ -41,6 +44,10 @@ gridContainer.style.gridTemplateRows = `repeat(${numberOfColumnsAndRows},1fr)`;
 
 drawSquares(gridContainer,numberOfColumnsAndRows) //Draw the squares in the grid.
 
+btnRainbow.addEventListener("click", () => {
+    isRainbowMode = !isRainbowMode 
+    console.log(isRainbowMode)
+});
 document.addEventListener("mousedown",(event) => {
     if(event.target.className == "square"){
         isDrawing = true;
@@ -50,10 +57,13 @@ document.addEventListener("mousedown",(event) => {
 })
 document.addEventListener("mouseup",() => {
     isDrawing = false;
+    if(!isDrawing){
+        hslColor =`hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`;
+    }
 })
 
 
-
+// Functions //
 function drawSquares(parent,number){ 
     while (parent.hasChildNodes()) {         ////Clean the Grid of squares before create new ones.
         parent.removeChild(parent.firstChild)   
@@ -61,15 +71,17 @@ function drawSquares(parent,number){
     for(let i=0; i< number*number;i++){ ///Create squares in the grid with a loop.
     let square = document.createElement(`div`);
     square.classList.add("square");
-    square.addEventListener("mousemove",(event) => { //Event to paint the squares, if "isDrawing" is true.
+    square.addEventListener("mouseenter",(event) => { //Event to paint the squares, if "isDrawing" is true.
         if(isDrawing){
             event.target.style.backgroundColor= hslColor;
+            createRandomColor()
         }
     })
     parent.appendChild(square);
     }
 }
-function createHslColor(event){
+function createHslColor(event){ // Create a new color.
+if(!isRainbowMode){
     if(event.target.className === "input-hue"){
         hueColor = event.target.value;
     } else if (event.target.className === "input-saturation"){
@@ -80,6 +92,14 @@ function createHslColor(event){
     console.log(hueColor);
     console.log(saturationColor);
     hslColor = `hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`
-    colorContainer.style.backgroundColor = hslColor;
+    colorContainer.style.backgroundColor = hslColor;  
 }
-
+}
+function createRandomColor() {
+    if(isRainbowMode){
+    let randomHueColor = Math.floor(Math.random() * 361);
+    let randomSaturation = 80;
+    let randomLightness = 50;
+    hslColor = `hsl(${randomHueColor},${randomSaturation}%,${randomLightness}%)`
+    }
+}
