20aa0462e1c0f00a13faf48fc35a78c39ea38a98
LucioFurnari <luciofurnari99@gmail.com>
2022-09-26 11:23:34 -0300

    Changes in the code, add clean mode, fix typo

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
+// DOM Elements //
 const gridContainer = document.querySelector(".grid-container"); //Grid container of the squares.
 const colorContainer = document.querySelector(".show-color");//Display the color in the page.
-// const numberInput = document.querySelector(".number-input");
 const btnGrid = document.querySelector(".grid-btn");
 const btnRainbow = document.querySelector(".rainbow-btn");
+const btnClean = document.querySelector(".clean-btn");
 
 const inputHueColor = document.querySelector(".input-hue");
 const inputSaturation = document.querySelector(".input-saturation");
 const inputLightness = document.querySelector(".input-lightness");
 
+// Variables //
 let numberOfColumnsAndRows = 16;
 let isDrawing = false;
 let isRainbowMode = false;
+let isCleanMode = false;
 
 let hueColor = inputHueColor.value; // Save the hue color.
 let saturationColor = inputSaturation.value; // Save the saturation.
 let lightnessColor = inputLightness.value; // Save the lightness.
 let hslColor =`hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`; // Save the actual color selected
 
+gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumnsAndRows},1fr)`;
+gridContainer.style.gridTemplateRows = `repeat(${numberOfColumnsAndRows},1fr)`;
 colorContainer.style.backgroundColor =  hslColor; // Show the actual color in the box.
+drawSquares(gridContainer,numberOfColumnsAndRows) //Draw the squares in the grid.
 
+
+// Event listener //
 inputHueColor.addEventListener("input", createHslColor);
 inputSaturation.addEventListener("input", createHslColor);
 inputLightness.addEventListener("input", createHslColor );
+
+btnClean.addEventListener("click",() => {
+    isRainbowMode = false;
+    isCleanMode = !isCleanMode;
+});
+
+btnRainbow.addEventListener("click", () => {
+    isCleanMode = false;
+    isRainbowMode = !isRainbowMode 
+    console.log(isRainbowMode)
+});
 
 btnGrid.addEventListener("click",() => {
     let changeSquares = parseInt(prompt("Ingrese la cantidad de squares (Maximo 100)"));
@@ -34,23 +53,11 @@ btnGrid.addEventListener("click",() => {
     }
 });
 
-// numberInput.addEventListener("input",(event) => {
-//     drawSquares(gridContainer,event.target.value);
-//     gridContainer.style.gridTemplateColumns = `repeat(${event.target.value},1fr)`;
-//     gridContainer.style.gridTemplateRows = `repeat(${event.target.value},1fr)`;
-// });
-gridContainer.style.gridTemplateColumns = `repeat(${numberOfColumnsAndRows},1fr)`;
-gridContainer.style.gridTemplateRows = `repeat(${numberOfColumnsAndRows},1fr)`;
-
-drawSquares(gridContainer,numberOfColumnsAndRows) //Draw the squares in the grid.
-
-btnRainbow.addEventListener("click", () => {
-    isRainbowMode = !isRainbowMode 
-    console.log(isRainbowMode)
-});
 document.addEventListener("mousedown",(event) => {
     if(event.target.className == "square"){
         isDrawing = true;
+        setCleanMode()
+        createRandomColor()
     } else {
         isDrawing = false;
     }
@@ -75,6 +82,7 @@ function drawSquares(parent,number){
         if(isDrawing){
             event.target.style.backgroundColor= hslColor;
             createRandomColor()
+            setCleanMode()
         }
     })
     parent.appendChild(square);
@@ -93,13 +101,17 @@ if(!isRainbowMode){
     console.log(saturationColor);
     hslColor = `hsl(${hueColor},${saturationColor}%,${lightnessColor}%)`
     colorContainer.style.backgroundColor = hslColor;  
-}
-}
+}};
 function createRandomColor() {
     if(isRainbowMode){
     let randomHueColor = Math.floor(Math.random() * 361);
     let randomSaturation = 80;
     let randomLightness = 50;
     hslColor = `hsl(${randomHueColor},${randomSaturation}%,${randomLightness}%)`
+    }
+}
+function setCleanMode() {
+    if(isCleanMode){
+        hslColor = `hsl(0,100%,100%)`;
     }
 }
